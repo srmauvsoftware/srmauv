e571020b42cb4bc870fd99a7b8c2439073a274a9
Harshit Nagar <hnagar.000@gmail.com>
2018-10-02 08:13:46 +0530

    testday2

--- Arduino/depth/depth.h
+++ Arduino/depth/depth.h
@@ -1,14 +1,19 @@
 
-#define TD1 10
-#define TD2 8
-#define TD3 4
-#define TD4 3
+#define TD1 2
+#define TD2 3
+#define TD3 6
+#define TD4 7
+
+#define TFR 10
+#define TFL 8
+#define TRR 4
+#define TRL 3
 
 #define PRESSURE 9 // US3000 pressure sensor
-#define LEAK 4
 #define LCD 44
 
 #define DEPTHCONSTANT 0.7
 #define WATER_LEAK_THRESH 400
 #define SURFACE 1500
-#define KILL 6
+
+#define ACT_DELAY 1000
--- Arduino/depth/depth.ino
+++ Arduino/depth/depth.ino
@@ -1,42 +1,67 @@
-/* 
- * rosserial Subscriber Example
- * Blinks an LED on callback
- */
-
 #include <ros.h>
 #include "depth.h"
 #include <thrusters/ThrusterMsg.h>
 #include <Servo.h>
 
-ros::NodeHandle  nh;
-thrusters::ThrusterMsg thruster;/
-Servo t1;
-Servo t2;
-Servo t3;
-Servo t4;
+ros::NodeHandle nh;
+thrusters::ThrusterMsg depthtThruster;
+thrusters::ThrusterMsg vectorThruster;
 
-void messageCb( const thrusters::ThrusterMsg& msg){
-  t1.write(msg.t1);
-  t2.write(msg.t2);
-  t3.write(msg.t3);
-  t4.write(msg.t4);
+Servo td1;
+Servo td2;
+Servo td3;
+Servo td4;
+
+Servo tfr;
+Servo tfl;
+Servo trr;
+Servo trl;
+
+void depthMessageCb( const thrusters::ThrusterMsg& msg){
+  td1.write(msg.t1);
+  td2.write(msg.t2);
+  td3.write(msg.t3);
+  td4.write(msg.t4);
 }
 
-ros::Subscriber<thrusters::ThrusterMsg> sub("/depthThruster", &messageCb );
+void vectorMessageCb( const thrusters::ThrusterMsg& msg)
+{
+  tfr.write(msg.t1);
+  tfl.write(msg.t2);
+  trr.write(msg.t3);
+  trl.write(msg.t4);
+}
+
+ros::Subscriber<thrusters::ThrusterMsg> depthSub("/depthThruster", &depthMessageCb );
+ros::Subscriber<thrusters::ThrusterMsg> vectorSub("/vectorThruster", &vectorMessageCb );
+
 
 void setup()
 { 
-  t1.attach(TD1);
-  t2.attach(TD2);
-  t3.attach(TD3);
-  t4.attach(TD4);
-  t1.write(1500);
-  t2.write(1500);
-  t3.write(1500);
-  t4.write(1500);
+  td1.attach(TD1);
+  td2.attach(TD2);
+  td3.attach(TD3);
+  td4.attach(TD4);
+  td1.write(1500);
+  td2.write(1500);
+  td3.write(1500);
+  td4.write(1500);
+  
+  delay(1000);
+  
+  tfr.attach(TFR);
+  tfl.attach(TFL);
+  trr.attach(TRR);
+  trl.attach(TRL);
+  tfr.write(1500);
+  tfl.write(1500);
+  trr.write(1500);
+  trl.write(1500);
+  
   delay(1000);
   nh.initNode();
-  nh.subscribe(sub);
+  nh.subscribe(depthSub);
+  nh.subscribe(vectorSub);
 }
 
 void loop()
