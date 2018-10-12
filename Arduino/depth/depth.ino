ab3456a4056a9de078fd6f69a9b11a93b50e102b
Pushkal Katara <katarapushkal@gmail.com>
2018-10-13 02:47:12 +0530

    Arduino Code for Pressure Sensor I2C

--- Arduino/depth/depth.ino
+++ Arduino/depth/depth.ino
@@ -1,92 +1,29 @@
-#include <ros.h>
-#include "depth.h"
-#include <thrusters/DepthThrusterMsg.h>
-#include <thrusters/VectorThrusterMsg.h>
 #include <Servo.h>
+#include "depth.h"
+#include <ros.h>
+#include <SimpleKalmanFilter.h>
+#include <Wire.h>
+#include "MS5837.h"
+#include <std_msgs/Float64.h>
 
-ros::NodeHandle nh;
-thrusters::DepthThrusterMsg depthThruster;
-thrusters::VectorThrusterMsg vectorThruster;
+ros::NodeHandle(nh);
+SimpleKalmanFilter pressureKF(1, 1, 0.01);
+MS5837 pressure_sensor;
 
-Servo td1;
-Servo td2;
-Servo td3;
-Servo td4;
+std_msgs::Float64 depth;
+ros::Publisher depth_pub("/depth", &depth);
 
-Servo tfr;
-Servo tfl;
-Servo trr;
-Servo trl;
-
-void depthMessageCb( const thrusters::DepthThrusterMsg& msg){
-  depthThruster = msg;
-}
-
-void vectorMessageCb( const thrusters::VectorThrusterMsg& msg){
-  vectorThruster = msg;
-}
-
-ros::Subscriber<thrusters::DepthThrusterMsg> depthSub("/depthThruster", &depthMessageCb );
-ros::Subscriber<thrusters::VectorThrusterMsg> vectorSub("/vectorThruster", &vectorMessageCb );
-
-void setup()
-{
-  initThrusters();
-  initNodes();
-}
-
-void loop()
-{
-  runDepthThrusters();
-  runVectorThrusters();
-  
-  nh.spinOnce();
-  delay(1);
-}
-
-void runVectorThrusters(){
-  tfr.write(vectorThruster.tfr);
-  tfl.write(vectorThruster.tfl);
-  trr.write(vectorThruster.trr);
-  trl.write(vectorThruster.trl);
-}
-
-void runDepthThrusters(){
-  td1.write(depthThruster.td1);
-  td2.write(depthThruster.td2);
-  td3.write(depthThruster.td3);
-  td4.write(depthThruster.td4);
-}
-
-void initNodes(){
+void setup(){
+  delay(1000);
   nh.initNode();
-  nh.subscribe(depthSub);
-  nh.subscribe(vectorSub);
+  Wire.begin();
+  pressure_sensor.init();
+  pressure_sensor.setFluidDensity(997);
 }
-void initThrusters(){
-  td1.attach(TD1);
-  td1.write(1500);
-  delay(1000);
-  td2.attach(TD2);
-  td2.write(1500);
-  delay(1000);
-  td3.attach(TD3);
-  td3.write(1500);
-  delay(1000);
-  td4.attach(TD4);
-  td4.write(1500);
-  delay(1000);
 
-  tfr.attach(TFR);
-  tfr.write(1500);
-  delay(1000);
-  tfl.attach(TFL);
-  tfl.write(1500);
-  delay(1000);
-  trr.attach(TRR);
-  trr.write(1500);
-  delay(1000);
-  trl.attach(TRL);
-  trl.write(1500);
-  delay(1000);
+void loop(){
+  pressure_sensor.read();
+  depth.data = pressureKF.updateEstimate(pressure_sensor.depth());
+  depth_pub.publish(&depth);
+  nh.spinOnce();
 }
